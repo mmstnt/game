cfa069d1ea058aa93055f34706d79fd98c8e0dd2
mmstnt <133971609+mmstnt@users.noreply.github.com>
2023-06-05 01:13:37 +0800

    第九次修改
    第九次修改

--- Assets/Code/Monster/boss.cs
+++ Assets/Code/Monster/boss.cs
@@ -67,9 +67,21 @@ public class boss : MonoBehaviour
 
     [Header("狀態")]  
     public Status status;
-	public enum Status{idle,actionModeChange,bossAttack01,death};
+	public enum Status{idle,actionModeChange,bossAttack01,bossAttack02,bossAttack03,death};
     public int bossMode;
     public GameObject bossAttack01Effect;
+    public float bossAttack01CD;
+    public GameObject bossAttack02Effect;
+    public float bossAttack02CD;
+    public int bossAttack02AmountMin;
+    public int bossAttack02AmountMax;
+    private int bossAttack02Amount;
+    public GameObject bossAttack03Effect;
+    public float bossAttack03CD;
+    public int bossAttack03AmountMin;
+    public int bossAttack03AmountMax;
+    private int bossAttack03Amount;
+
 
     private void actionMode(){
 		switch(status){
@@ -80,7 +92,18 @@ public class boss : MonoBehaviour
                 status = Status.idle;
             break;
             case Status.bossAttack01:
-                bossAttack01();
+                Invoke("bossAttack01",0.1f);
+                status = Status.idle;
+            break;
+            case Status.bossAttack02:
+                bossAttack02Amount = UnityEngine.Random.Range(bossAttack02AmountMin,(bossAttack02AmountMax+1));
+                Invoke("bossAttack02",0.1f);
+                status = Status.idle;
+            break;
+            case Status.bossAttack03:
+                bossAttack03Amount = UnityEngine.Random.Range(bossAttack03AmountMin,(bossAttack03AmountMax+1));
+                Invoke("bossAttack03",0.1f);
+                status = Status.idle;
             break;
 			case Status.death:
                 InvokeRepeating("deathAnimation", 0, 0.05f);
@@ -92,30 +115,69 @@ public class boss : MonoBehaviour
 
     private void actionModeChange()
     {
-        bossMode = UnityEngine.Random.Range(1,1);
+        bossMode = UnityEngine.Random.Range(1,4);
         switch(bossMode)
         {
             case 1:
                 status = Status.bossAttack01;
             break;
             case 2:
-
+                status = Status.bossAttack02;
+            break;
+            case 3:
+                status = Status.bossAttack03;
             break;
         }
     }
 
+    private void actionModePrepare()
+    {
+        status = Status.actionModeChange;
+    }
+
     private void bossAttack01()
     {
-        status = Status.idle;
         Instantiate(bossAttack01Effect,this.gameObject.transform.position,Quaternion.Euler(-90,0,0));
-        status = Status.actionModeChange;
+        Invoke("actionModePrepare",bossAttack01CD);
+    }
+
+    private void bossAttack02()
+    {
+        Vector3 v = playerSite.position;
+        v.x += UnityEngine.Random.Range(-8.0f,8.0f);
+        v.y += UnityEngine.Random.Range(2.0f,8.0f);
+        Instantiate(bossAttack02Effect,v,Quaternion.Euler(0,0,0));
+        bossAttack02Amount--;
+        if(bossAttack02Amount > 0)
+        {
+            Invoke("bossAttack02",0.5f);
+        }
+        else
+        {
+            Invoke("actionModePrepare",bossAttack02CD);
+        }
+    }
+
+    private void bossAttack03()
+    {
+        Vector3 v = this.gameObject.transform.position;
+        v.x = playerSite.position.x;
+        Instantiate(bossAttack03Effect,v,Quaternion.Euler(0,0,0));
+        bossAttack03Amount--;
+        if(bossAttack03Amount > 0)
+        {
+            Invoke("bossAttack03",0.5f);
+        }
+        else
+        {
+            Invoke("actionModePrepare",bossAttack03CD);
+        }
     }
 
     private void deathAnimation()
     {
         sp.color -= new Color32(0,0,0,16);
     }
-
 
     private void death()
     {
